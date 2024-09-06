3ec83a3aaf71f544aa8de528598697827cd8c515
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-06 19:16:44 +0530

    feat: Update the blade file for create operation

---
+++ resources/views/notes/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+    <h1>Add Note</h1>
+    <form action="{{ route('notes.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Add Note</button>
+    </form>
+@endsection
