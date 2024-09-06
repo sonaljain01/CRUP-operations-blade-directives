2de7e91fb49d5b632b60437fedb2e33deb645752
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-06 18:50:09 +0530

    feat: Create the basic structure of the page

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Notes App</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Notes</a>
+    </nav>
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+</body>
+</html>
