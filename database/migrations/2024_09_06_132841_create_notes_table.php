443c60e5543a6327d86485ae1d9da39b28d18abe
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-06 19:02:17 +0530

    feat: Update the database file according to our requirement

--- database/migrations/2024_09_06_132841_create_notes_table.php
+++ database/migrations/2024_09_06_132841_create_notes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('status')->default('Completed');
             $table->timestamps();
         });
     }
